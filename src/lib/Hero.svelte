d93383bfdf1a4c9ad206268d0899a1d98f84c99b
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-05-26 08:57:23 +0800

    feat(`Hero`): introduce support system

---
+++ src/lib/Hero.svelte
@@ -0,0 +1,41 @@
+<div class="grid grid-rows-2 grid-cols-5 gap-2 p-5 dark:text-black">
+    <div class="card p-4 dark:bg-primary-500 col-span-3">
+        <header class="card-header h1">So what even is this?</header>
+        <section class="p-4">
+            Well, this is my proposed support system for helping the art community! 
+            Here, I wanted to create a sort of <b>freedom wall dedicated to artists and to everyone interested in art</b>.
+            After all, the internet is a good way to connect people from all across the globe.
+        </section>
+    </div>
+    <div class="card p-4 dark:bg-secondary-500 col-span-2">
+        <header class="card-header h1">But why?</header>
+        <section class="p-4">
+            Well, to <b>unite the art community</b> of course! Artists from all across the globe can gather and share their thoughts here.
+        </section>
+    </div>
+    <div class="card p-4 dark:bg-tertiary-500 col-span-2">
+        <header class="card-header h1">What it do?</header>
+        <section class="p-4">
+            Lots of stuff! 
+            It can be a <b>chat forum for any art topic</b> under the sun, it can be a <b>tutorials page for budding artists</b> wishing to learn "how to art", it can even be a place where artists ask for <b>constructive criticism from other artists</b>! The limits are endless.
+        </section>
+    </div>
+    <div class="card p-4 dark:bg-tertiary-50 col-span-3">
+        <header class="card-header h1">For what?</header>
+        <section class="p-4">
+            Everyday, new ideas are being brought by artists yet are never received by other fellow enjoyers in the community.
+            Those <b>brilliant ideas must be shared with the world</b>, thus the creation of this prototype.
+        </section>
+        <section class="card-footer">
+            Most importantly though, this support system would hopefully act as a <b>central hub and a safe space for artists to grow and develop their skills amidst the cruelties of reality</b>.
+        </section>
+    </div>
+</div>
+
+        <!--
+        <header class="card-header h1">Let's go!</header>
+        <section class="p-4">
+            The art world is brimming with new and exciting ideas every single day.
+            Those <b>brilliant ideas must be shared with the world</b>, thus the creation of this prototype.
+        </section>
+        -->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,9 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import Hero from "$lib/Hero.svelte";
+</script>
+
+<Hero/>
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
